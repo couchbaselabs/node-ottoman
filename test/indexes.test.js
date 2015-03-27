2b9224cbaa9df98491556f320db51bd8d45fe97c
Brett Lawson <brett19@gmail.com>
2015-03-27 10:08:06 -0300

    More refdoc testing.

--- test/indexes.test.js
+++ test/indexes.test.js
@@ -51,4 +51,96 @@ describe('Model Indexes', function() {
     _indexTest.call(this, 'refdoc', done);
   });
 
+  it('should fail to have two identical refdoc keys', function(done) {
+    var modelId = H.uniqueId('model');
+
+    var TestMdl = ottoman.model(modelId, {
+      name: 'string',
+      company: 'string'
+    }, {
+      index: {
+        findByName: {
+          type: 'refdoc',
+          by: 'name'
+        },
+        findByCompany: {
+          type: 'refdoc',
+          by: 'company'
+        }
+      }
+    });
+
+    ottoman.ensureIndices(function(err) {
+      assert.isNull(err);
+
+      var x = new TestMdl();
+      x.name = 'Frank';
+      x.company = 'Couchbase';
+      var y = new TestMdl();
+      y.name = 'George';
+      y.company = 'Couchbase';
+
+      x.save(function(err) {
+        assert.isNull(err);
+
+        y.save(function(err) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+    });
+  });
+
+  it('should succeed with a previously changed refdoc key', function(done) {
+    var modelId = H.uniqueId('model');
+
+    var TestMdl = ottoman.model(modelId, {
+      name: 'string',
+      company: 'string'
+    }, {
+      index: {
+        findByName: {
+          type: 'refdoc',
+          by: 'name'
+        },
+        findByCompany: {
+          type: 'refdoc',
+          by: 'company'
+        }
+      }
+    });
+
+    ottoman.ensureIndices(function(err) {
+      assert.isNull(err);
+
+      var x = new TestMdl();
+      x.name = 'Frank';
+      x.company = 'Couchbase';
+      var y = new TestMdl();
+      y.name = 'Frank';
+      y.company = 'Google';
+
+      x.save(function(err) {
+        assert.isNull(err);
+
+        x.name = 'George';
+        x.save(function(err) {
+          assert.isNull(err);
+
+          y.save(function(err) {
+            assert.isNull(err);
+
+            y.name = 'Bob';
+            y.company = 'VMWare';
+            y.save(function(err) {
+              assert.isNull(err);
+              
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
 });
