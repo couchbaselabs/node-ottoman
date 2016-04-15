df6f7d3777ab7d997627262bf4bc59d983a0f144
Moxious <moxious@oldhat.org>
2016-04-15 12:54:38 -0400

    fix for bug in ottoman issue #51 types in arrays
    fixed bug where nested array types were handled improperly
    
    better testing for type checking in arrays

--- test/basic.test.js
+++ test/basic.test.js
@@ -22,6 +22,24 @@ describe('Models', function() {
     });
   });
 
+  it('should fail with an invalid type specified in array', function() {
+    var modelId = H.uniqueId('model');
+    assert.throws(function() {
+      ottoman.model(modelId, {
+        'test': ['stringxxxx']
+      });
+    });
+  });
+
+  it('should fail when an array type has more than one member', function() {
+    var modelId = H.uniqueId('model');
+    assert.throws(function () {
+      ottoman.model(modelId, {
+        'someField': ['string', 'string']
+      });
+    });  
+  });
+
   it('should understand all basic types', function() {
     var modelId = H.uniqueId('model');
     ottoman.model(modelId, {
@@ -30,6 +48,36 @@ describe('Models', function() {
       'int': 'integer',
       'bool': 'boolean'
     });
+  });
+
+  it('should understand all basic types in both schema formats, flat and object', function () {
+    var modelId = H.uniqueId('model');
+    
+    var counter = 0;
+    function mkName(str) {
+      return modelId + '_' + str + '_' + (++counter); 
+    }
+    
+    var types = ['string', 'number', 'integer', 'boolean'];
+    
+    types.forEach(function(type) {
+      ottoman.model(mkName(type), {
+        'someField': type
+      });
+      
+      ottoman.model(mkName(type), {
+        'someField': { type: type }
+      });
+      
+      ottoman.model(mkName(type), {
+        'someFieldArray': [type]  
+      });
+      
+      ottoman.model(mkName(type), {
+        'someFieldArray': [{ type: type }]
+      });
+    });
+
   });
 
   it('should serialize basic types properly', function () {
