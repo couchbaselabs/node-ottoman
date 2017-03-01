ae13a8009e51c5ebe168109ff87d71e476f413e3
Christian Käslin <c.kaeslin@gmx.ch>
2017-03-01 11:48:36 +0100

    add another test for ensure correct model behavior with multiple linked ottomans

--- test/namespace.test.js
+++ test/namespace.test.js
@@ -8,7 +8,6 @@ describe('Namespace', function () {
   // Add long timeout in case of slow response on CI build servers.
   this.timeout(10000);
 
-  // test ottoman reference between ottomans with passed models
   it('should be able to make reference between namespaces, with passed models ',
   function(done) {
     var modelId1 = H.uniqueId('model');
@@ -33,12 +32,10 @@ describe('Namespace', function () {
       m2.save(function (err) {
         expect(err).to.be.null;
         done();
-      })
+      });
     });
 
   });
-
-  // test ottoman references between ottoman without passed models
 
   it('should fail to make reference between namespaces, without passed models ',
   function(done) {
@@ -65,6 +62,55 @@ describe('Namespace', function () {
       }).to.throw(TypeError);
       done();
     });
+  });
+
+
+  /* this tests explicit if that there is no connection between
+   * models in different ottomans
+   * if all the models would refer to the same object it would be
+   * possible to create a model with the same name in a different namespaces
+   * with the same shared base ottoman
+   */
+  it('should be able use a base ottoman more then once',
+  function(done) {
+    var modelId1 = H.uniqueId('model');
+    var modelId2 = H.uniqueId('model');
+    var ottomanA = H.setupOttoman('A');
+    var Model1 = ottomanA.model(modelId1, {
+      'str': 'string'
+    });
+
+    var ottomanB = H.setupOttoman('B', ottomanA.models, ottomanA.types);
+    var Model2 = ottomanB.model(modelId2, {
+      'str': 'string',
+      'model1': {
+        'ref': Model1
+      }
+    });
+
+    var ottomanC = H.setupOttoman('C', ottomanA.models, ottomanA.types);
+    // explicit test to set same model name (modelId2)
+    var Model3 = ottomanC.model(modelId2, {
+      'str': 'string',
+      'model1': {
+        'ref': Model1
+      }
+    });
+
+    var m1 = new Model1({ str: 'test1' });
+    m1.save(function (err) {
+      expect(err).to.be.null;
+      var m2 = new Model2({ str: 'test2', model1: m1 });
+      m2.save(function (err) {
+        expect(err).to.be.null;
+        var m3 = new Model3({ str: 'test3', model1: m1 });
+        m3.save(function (err) {
+          expect(err).to.be.null;
+          done();
+        });
+      });
+    });
+
   });
 
 });
