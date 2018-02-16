6a64012dd24a73cd5abe7e210cd0c6d5c88c0ea1
Will Bartlett <will118@gmail.com>
2018-02-16 11:23:25 +0000

    Export type fix (#1)
    * Exports namespace in .d.ts file.
    
    * Reindents the .d.ts file, corrects some types.
    
    * Adds methods to StoreAdapters.

--- index.d.ts
+++ index.d.ts
@@ -3,65 +3,67 @@
 import { Bucket, CouchbaseError  } from "couchbase";
 
 declare namespace OttomanJS {
-    class StoreAdapter {
-        constructor(bucket: Bucket)
-    }
+  class StoreAdapter {
+    constructor (bucket: Bucket)
+    store (key: string, data: object, cas: object, callback: any): any
+  }
 
-    interface Model {
-    }
+  class CbStoreAdapter {
+    constructor (bucket: Bucket)
+    store (key: string, data: object, cas: object, callback: any): any
+  }
 
-    interface TypeDef {
-    }
+  interface Model {
+  }
 
-    interface OttomanOptions {
-        bucket?: Bucket
-        store?: StoreAdapter
-        namespace?: string
-    }
+  interface TypeDef {
+  }
 
-    interface Schema {
+  interface OttomanOptions {
+    bucket?: Bucket
+    store?: StoreAdapter
+    namespace?: string
+  }
 
-    }
+  interface Schema {
 
-    interface Indices {
-    }
+  }
 
-    interface GetByIdOptions {
-    }
+  interface Indices {
+  }
 
-    interface CreateOptions {
-    }
+  interface GetByIdOptions {
+  }
 
-    type CreateCallback = (error: CouchbaseError | null, document: ModelInstance | undefined) => void
+  interface CreateOptions {
+  }
+
+  type CreateCallback = (error: CouchbaseError | null, document: ModelInstance | undefined) => void
     type GetByIdCallback = (error: CouchbaseError | null, model: ModelInstance | undefined) => void
     type SaveCallback = (error: CouchbaseError | null, response: ModelInstance | undefined) => void
 
     export interface ModelInstance {
-        save (callback: SaveCallback): void
+      save (callback: SaveCallback): void
     }
 
-    interface Model {
-        getById (id: string, options: GetByIdOptions | undefined, callback: GetByIdCallback): void
-        create (id: object, options: CreateOptions | undefined, callback: CreateCallback): void
-    }
+  interface Model {
+    getById (id: string, options: GetByIdOptions | undefined, callback: GetByIdCallback): void
+    create (id: object, callback: CreateCallback): void
+  }
 
-    class Ottoman {
-        bucket: Bucket
-        readonly namespace: string
-        readonly store: StoreAdapter
-        readonly models: { [key: string]: Model }
-        readonly types: { [key: string]: TypeDef | undefined }
-        readonly delayedBind: { [key: string]: Function | undefined }
-        readonly plugins: Array<[Function, object]>
+  class Ottoman {
+    bucket: Bucket
+    readonly namespace: string
+    readonly store: StoreAdapter
+    readonly models: { [key: string]: Model }
+    readonly types: { [key: string]: TypeDef | undefined }
+    readonly delayedBind: { [key: string]: Function | undefined }
+    readonly plugins: Array<[Function, object]>
 
-        constructor (options: OttomanOptions)
+    constructor (options: OttomanOptions)
 
-        model(key: string, schema: Schema, index: Indices): Model
-    }
+    model (key: string, schema: Schema, index: Indices): Model
+  }
 }
 
-declare module 'ottoman' {
-    // bucket and model intentionally not exposed (singletons are baaaaad.)
-    let Ottoman: OttomanJS.Ottoman
-    let StoreAdapter: OttomanJS.StoreAdapter
-}
+export = OttomanJS
