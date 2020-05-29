c3819f333c946aac9955a2857d210571853a2369
Chao <chao@generalsoftwareinc.com>
2020-05-29 16:01:03 -0400

    Auto generate model key

--- src/model/model-base.ts
+++ src/model/model-base.ts
@@ -1,11 +1,11 @@
-import {validateSchema} from "..";
-import { save } from "../handler/save";
+import { validateSchema } from '..';
+import { save } from '../handler/save';
 
 export class ModelBase {
-    constructor(public __data, public __collection, public __schema) {}
+  constructor(public __data, public __collection, public __schema) {}
 
-    save(key) {
-        validateSchema(this.__data, this.__schema);
-        return save(key, this.__data, this.__collection);
-    }
+  save(key) {
+    validateSchema(this.__data, this.__schema);
+    return save(key, this.__data, this.__collection);
+  }
 }
