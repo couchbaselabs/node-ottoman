94e7cea10b17e691546556b92c3b219f552127cb
Chao <chao@generalsoftwareinc.com>
2020-06-17 14:10:39 -0400

    test(extract-select): add test case to getProjectionFields method

--- src/utils/populate/can-be-populated.ts
+++ src/utils/populate/can-be-populated.ts
@@ -5,7 +5,7 @@ export const canBePopulated = (populate: string, fields: string[]): boolean => {
   if (fields.includes(populate)) {
     return true;
   }
-  throw new Error(
-    `Unable to populate field "${populate}", it is not available on select clause [${fields.join(', ')}]`,
-  );
+
+  console.warn(`Unable to populate field "${populate}", it is not available on select clause [${fields.join(', ')}]`);
+  return false;
 };
