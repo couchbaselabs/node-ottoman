9f6da1360ec08b1570c41dd9b2653c7e552c0589
gsi-alejandro <alejandro@gsoftinnovation.com>
2020-07-02 15:55:03 -0400

    test(queries): adding integration test for queries

--- __test__/indexes-query.spec.ts
+++ __test__/indexes-query.spec.ts
@@ -1,42 +1,44 @@
-import { Schema, model } from '../lib';
+import { Schema, model, ensureIndexes } from '../lib';
+import { delay } from './testData';
 
-// const generateMockData = async () => {
-//   const userSchema = new Schema({
-//     name: String,
-//   });
-//   userSchema.queries = {
-//     myPosts: {
-//       of: 'Post',
-//       by: 'user',
-//     },
-//   };
-//   const User = model('User', userSchema);
-//   const postSchema = new Schema({
-//     user: { type: userSchema, ref: 'User' },
-//     title: String,
-//     body: String,
-//   });
-//   const Post = model('Post', postSchema);
-//   const user = new User({ name: 'ottoman-user' });
-//   await user.save();
-//   const post = new Post({
-//     title: 'The new post',
-//     body: 'This is my new post',
-//     user: user.id,
-//   });
-//   await post.save();
-//   await ensureIndexes();
-//   return { user, post };
-// };
+const generateMockData = async () => {
+  const userSchema = new Schema({
+    name: String,
+  });
+  userSchema.queries = {
+    myPosts: {
+      of: 'Post',
+      by: 'user',
+    },
+  };
+  const User = model('User', userSchema);
+  await ensureIndexes();
+  const postSchema = new Schema({
+    user: { type: userSchema, ref: 'User' },
+    title: String,
+    body: String,
+  });
+  const Post = model('Post', postSchema);
+  const user = new User({ name: 'ottoman-user' });
+  await user.save();
+  const post = new Post({
+    title: 'The new post',
+    body: 'This is my new post',
+    user: user.id,
+  });
+  await post.save();
+  return { user, post };
+};
 
 describe('Mode Query Index Tests', () => {
-  // test('Test schema query structure', async () => {
-  //   const { user } = await generateMockData();
-  //   const results = await user.myPosts();
-  //   expect(results.rows).toBeDefined();
-  //   expect(results.rows.length > 0).toBeTruthy();
-  //   expect(results.rows[0].user).toBe(user.id);
-  // });
+  test('Test schema query structure', async () => {
+    const { user } = await generateMockData();
+    await delay(2500);
+    const results = await user.myPosts();
+    expect(results.rows).toBeDefined();
+    expect(results.rows.length > 0).toBeTruthy();
+    expect(results.rows[0].user).toBe(user.id);
+  });
 
   test('Test schema query: check wrong structure', async () => {
     const run = () => {
