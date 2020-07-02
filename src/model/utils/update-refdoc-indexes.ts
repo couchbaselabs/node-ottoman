c0ae4366226ee64fe3f8e21fe038454d6050b81e
gsi-alejandro <alejandro@gsoftinnovation.com>
2020-07-02 14:22:18 -0400

    refactor(ci): not emit warning if env.CI is true

--- src/model/utils/update-refdoc-indexes.ts
+++ src/model/utils/update-refdoc-indexes.ts
@@ -1,13 +1,21 @@
 export const updateRefdocIndexes = (refKeys: { add: string[]; remove: string[] }, key: string | null, collection) => {
   for (const ref of refKeys.add) {
     if (ref.length < 250) {
-      collection.insert(ref, key).catch((e) => console.warn(e));
+      collection.insert(ref, key).catch((e) => {
+        if (!process.env.CI || (process.env.CI && process.env.CI.toLowerCase() !== 'true')) {
+          console.warn(e);
+        }
+      });
     } else {
       console.log(`Unable to store refdoc index for ${ref}, Maximum key size is 250 bytes`);
     }
   }
 
   for (const ref of refKeys.remove) {
-    collection.remove(ref).catch((e) => console.warn(e));
+    collection.remove(ref).catch((e) => {
+      if (!process.env.CI || (process.env.CI && process.env.CI.toLowerCase() !== 'true')) {
+        console.warn(e);
+      }
+    });
   }
 };
