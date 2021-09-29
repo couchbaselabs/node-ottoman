4128b007b4003527e17c36c819cb736c03c88e7c
gsi-alejandro <alejandro@gsoftinnovation.com>
2021-09-29 17:57:43 -0400

    feat(sdk types): exposing some sdk types to use directly from Ottoman

--- src/couchbase.ts
+++ src/couchbase.ts
@@ -1,3 +1,26 @@
 import * as Couchbase from 'couchbase';
-
 export { Couchbase };
+
+export {
+  BucketSettings,
+  CreateBucketOptions,
+  CertificateAuthenticator,
+  ICreateBucketSettings,
+  BucketManager,
+  CollectionManager,
+  NodeCallback,
+  Bucket,
+  QueryIndexManager,
+  Cluster,
+  DropCollectionOptions,
+  CreateScopeOptions,
+  CreateCollectionOptions,
+  DropScopeOptions,
+  RequestSpan,
+  DropBucketOptions,
+  Collection,
+  QueryOptions,
+  QueryProfileMode,
+  MutationState,
+  QueryScanConsistency,
+} from 'couchbase';
