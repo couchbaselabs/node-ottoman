43189d21999e1ecbc75082542cc3e6737787e2b2
gsi-alejandro <alejandro@gsoftinnovation.com>
2022-02-13 17:33:29 -0500

    ci(semver): adding automation for semantic-release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
