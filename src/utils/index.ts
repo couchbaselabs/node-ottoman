c632f23848fdb6c084bdb4f956df5ab734c70e34
gsi-alejandro <alejandro@gsoftinnovation.com>
2022-03-13 23:29:47 -0400

    feat(ottoman): add support for nested modelKey
    Closes: "#638"

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export { isDocumentNotFoundError } from './is-not-found';
 export { VALIDATION_STRATEGY } from './validation.strategy';
 export { is, isSchemaTypeSupported, isSchemaFactoryType } from './is-type';
 export { set } from './environment-set';
+export { setValueByPath } from './setValueByPath';
+export { getValueByPath } from './getValueByPath';
