d0cfc4f577d82fd6ad51e2736b0a521cbc188bd3
gsi-alejandro <alejandro@gsoftinnovation.com>
2022-05-17 12:03:26 -0400

    fix(index): update regex for clean index name

---
+++ __test__/query-array.spec.ts
@@ -0,0 +1,39 @@
+import { model, Schema, SearchConsistency, getDefaultInstance, set } from '../src';
+import { startInTest } from './testData';
+
+describe('Query Builder Array', () => {
+  const houseSchema = new Schema({
+    title: String,
+    address: {
+      line1: String,
+      line2: String,
+      state: String,
+    },
+    numbers: [Number],
+  });
+
+  test('Basics', async () => {
+    set('DEBUG', true);
+    const House = model('House', houseSchema);
+    await startInTest(getDefaultInstance());
+
+    const house = new House({
+      title: 'Beach House',
+      address: { line1: 'miami beach', state: 'FL' },
+      numbers: [1, 5, 10, 156788],
+    });
+    await house.save();
+
+    const filter = {
+      title: 'Beach House',
+      $any: {
+        $expr: [{ search: 'numbers' }],
+        $satisfies: { search: { $in: [10] } },
+      },
+    };
+
+    const results = await House.find(filter, { consistency: SearchConsistency.LOCAL });
+    console.log(results);
+    expect(results.rows.length).toBeGreaterThanOrEqual(1);
+  });
+});
