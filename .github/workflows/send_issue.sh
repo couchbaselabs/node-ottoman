25ad5508fd923a0c18744588b1215c9a3fa875ee
Prajwal Pai <prajwalpai@Prajwals-MacBook-Pro.local>
2022-11-25 12:10:53 +0530

    add issue.yaml

---
+++ .github/workflows/send_issue.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+ISSUE_TITLE="$1"
+ISSUE_BODY="$2"
+ISSUE_URL="$3"
+RUN_ID="$4"
+SLACK_WEBHOOK_URL="$5"
+API_TOKEN_TO_GITHUB="$6"
+cat <<EOT > /tmp/slack_message.json
+{
+    "type": "mrkdwn",
+    "text": "New Issue Opened on Node Ottoman ",
+    "attachments": [
+        {
+            "fallback": "Issue",
+            "color": "#36a64f",
+            "pretext": "Newly Opened Issue : ${ISSUE_TITLE} \n \n ${ISSUE_BODY} \n \n For more details about issue: ${ISSUE_URL} ",
+            "footer_icon": "https://www.couchbase.com/webfiles/1629373386042/images/favicon.ico",
+            "ts": ${TIME_STAMP}
+        }
+    ]
+}
+EOT
+
+cat /tmp/slack_message.json
+curl -s -X POST -H 'Content-type: application/json' --data @/tmp/slack_message.json ${SLACK_WEBHOOK_URL}
