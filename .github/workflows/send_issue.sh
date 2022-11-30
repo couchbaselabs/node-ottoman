f7c5c72007d287c5145f78d72da2eea7b2738326
Prajwal Pai <prajwalpai@Prajwals-MacBook-Pro.local>
2022-11-30 11:07:56 +0530

    update issue.yaml

--- .github/workflows/send_issue.sh
+++ .github/workflows/send_issue.sh
@@ -5,22 +5,23 @@
 RUN_ID="$1"
 SLACK_WEBHOOK_URL="$2"
 API_TOKEN_TO_GITHUB="$3"
-# cat <<EOT > /tmp/slack_message.json
-# {
-#     "type": "mrkdwn",
-#     "text": "New Test Issue Opened on Node Ottoman ",
-#     "attachments": [
-#         {
-#             "fallback": "Test Ottoman Issue",
-#             "color": "#36a64f",
-#             "pretext": "Newly Opened Issue : ${ISSUE_TITLE} \n \n ${ISSUE_BODY} \n \n For more details about issue: ${ISSUE_URL} ",
-#             "footer_icon": "https://www.couchbase.com/webfiles/1629373386042/images/favicon.ico",
-#             "ts": ${TIME_STAMP}
-#         }
-#     ]
-# }
-# EOT
+ISSUE_TITLE = "THIS IS A TEST ISSUE"
+cat <<EOT > /tmp/slack_message.json
+{
+    "type": "mrkdwn",
+    "text": "New Test Issue Opened on Node Ottoman ",
+    "attachments": [
+        {
+            "fallback": "Test Ottoman Issue",
+            "color": "#36a64f",
+            "pretext": "Newly Opened Issue : ${ISSUE_TITLE} \n \n \n \n For more details about issue: ",
+            "footer_icon": "https://www.couchbase.com/webfiles/1629373386042/images/favicon.ico",
+            "ts": ${TIME_STAMP}
+        }
+    ]
+}
+EOT
 
-# cat /tmp/slack_message.json
+cat /tmp/slack_message.json
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, Ottoman! This is a test."}' ${SLACK_WEBHOOK_URL}
+curl -X POST -H 'Content-type: application/json' --data @/tmp/slack_message.json ${SLACK_WEBHOOK_URL}
