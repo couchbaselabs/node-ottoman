c9af0b19d6d5e3c20f29a444c1542ca576417eaa
gsi-alejandro <alejandro@generalsoftwareinc.com>
2023-12-12 13:18:43 -0500

    ci: disabling tests with `maybe` function for CI environments
    The `maybe` function has been introduced in the `fts.spec.ts` test file to skip tests when in a continuous integration environment. The beforeAll hook using delay has been removed, since test cases are either run or skipped based on the process environment, thereby improving test execution efficiency

--- __test__/fts.spec.ts
+++ __test__/fts.spec.ts
@@ -1,25 +1,22 @@
 import { searchQuery, SearchQuery } from '../src';
-import { delay } from './testData';
 
-beforeAll(async () => {
-  await delay(15000);
-});
+const maybe = process.env.CI ? test.skip : test;
 
-test('fts match results', async () => {
+maybe('fts match results', async () => {
   const result = await searchQuery('hotels', SearchQuery.match('Gillingham'), { limit: 5 });
   expect(result).toBeDefined();
   expect(result.rows.length).toBeGreaterThanOrEqual(1);
   expect(result.rows[0].id).toBeDefined();
 });
 
-test('fts matchPhrase basic', async () => {
+maybe('fts matchPhrase basic', async () => {
   const result = await searchQuery('hotels', SearchQuery.matchPhrase('Medway Youth Hostel'), { limit: 5 });
   expect(result).toBeDefined();
   expect(result.rows.length).toBeGreaterThanOrEqual(1);
   expect(result.rows[0].id).toBe('hotel_10025');
 });
 
-test('fts conjuncts results', async () => {
+maybe('fts conjuncts results', async () => {
   const query = SearchQuery.conjuncts(SearchQuery.match('Berkeley'), SearchQuery.matchPhrase('luxury hotel'));
   const result = await searchQuery('hotels', query);
   expect(result).toBeDefined();
@@ -27,7 +24,7 @@ test('fts conjuncts results', async () => {
   expect(result.rows[0].id).toBeDefined();
 });
 
-test('fts disjunction results', async () => {
+maybe('fts disjunction results', async () => {
   const query = SearchQuery.disjuncts(SearchQuery.match('Louvre'), SearchQuery.match('Eiffel'));
   const result = await searchQuery('hotels', query);
   expect(result).toBeDefined();
