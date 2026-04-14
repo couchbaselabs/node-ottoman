d91f1902cfd175bc3a74eaee405a659e7b069259
prajwal-pai77 <prajwal.pai@couchbase.com>
2026-04-14 11:20:24 +0530

    undo change and add gtag

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -14,17 +14,6 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-headTags: [
-    {
-      tagName: 'script',
-      attributes: {},
-      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//metrics.couchbase.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MVPNN2');`,
-    },
-  ],
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
@@ -54,6 +43,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        googleTagManager: {
+          containerId: 'GTM-MVPNN2',
+        },
+        gtag: {
+          trackingID: 'G-CVKKEY0D6B',
+          anonymizeIP: true,
         },
       }),
     ],
